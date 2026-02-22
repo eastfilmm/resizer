ce21c956db072ded263d6281f65be9f28fbc1c3a
Dongpil Jo <ehdvlf001@knu.ac.kr>
2026-02-22 12:15:24 +0900

    feat: vercel Analytics 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata, Viewport } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 import StyledComponentsRegistry from "@/lib/styled-components-registry";
 import { getSiteUrl, getSiteUrlObject } from "@/utils/siteConfig";
@@ -140,6 +141,7 @@ export default function RootLayout({
         <StyledComponentsRegistry>
           {children}
         </StyledComponentsRegistry>
+        <Analytics />
       </body>
     </html>
   );
