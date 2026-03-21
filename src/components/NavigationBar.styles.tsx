f1ae7f4553457323ab732b662b080e4f6775ec63
Dongpil Jo <ehdvlf001@knu.ac.kr>
2026-03-21 12:35:32 +0900

    fix: nav bar css 수정

--- src/components/NavigationBar.styles.tsx
+++ src/components/NavigationBar.styles.tsx
@@ -35,7 +35,7 @@ export const PanelContentWrapper = styled.div<{ $isVisible: boolean }>`
 `;
 
 export const NavContainer = styled.div`
-  height: 68px;
+  height: 64px;
   background-color: #e0e0e0;
   display: flex;
   width: 100%;
